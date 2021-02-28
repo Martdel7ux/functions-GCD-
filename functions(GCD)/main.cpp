87095a99fd35a8eac0ceb1e4c17d40f5562a02cc
Martin Ahoto <martinahoto4@gmail.com>
2021-02-28 21:35:39 +0200

    Initial Commit

---
+++ functions(GCD)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  functions(GCD)
+//
+//  Created by MARTIN on 28/02/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
