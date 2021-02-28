f6d352f645b67185c0b18865f29733a62bbb2696
Martin Ahoto <martinahoto4@gmail.com>
2021-03-01 00:29:09 +0200

    implemented other functions to the code to make it more distinct

--- functions(GCD)/main.cpp
+++ functions(GCD)/main.cpp
@@ -9,6 +9,45 @@
 
 using namespace std;
 
+
+double divide(int num1,int num2)
+{
+    int divide;
+    
+    divide= num1/num2;
+    
+    return divide;
+}
+
+int average(int num1,int num2)
+{
+    int average;
+    average=(num1+num2)/2;
+    
+    return average;
+}
+
+int sum(int num1,int num2)
+{
+    int sum;
+    sum=num1+num2;
+    
+    return sum;
+}
+
+int max(int num1,int num2)
+{
+    int result;
+    if(num1>num2)
+    {
+         result = num1;
+    }
+    else
+        result=num2;
+    
+    return result;
+}
+
 int gcd(int num1,int num2)
 {
     int gcd=1;
@@ -42,7 +81,10 @@ int main()
     cout<<"enter second number here:"<<endl;
     cin>>n2;
     
-    
+    cout<<n1<<"/"<<n2<<"="<<divide(n1,n2)<<endl;
+    cout<<"average is:"<<average(n1,n2)<<endl;
+    cout<<"sum of two numbers is:"<<sum(n1,n2)<<endl;
+    cout<<"max of two numbers is:"<<max(n1,n2)<<endl;
     cout<<"GCD is:"<<gcd(n1,n2)<<endl;
     
     return 0;
