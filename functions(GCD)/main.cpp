a4dd522ad0dbf0a5df04cc5f4f7c1315ad654140
Martin Ahoto <martinahoto4@gmail.com>
2021-02-28 22:59:13 +0200

    a sample code for function that calculates the gcd of two integers

--- functions(GCD)/main.cpp
+++ functions(GCD)/main.cpp
@@ -7,8 +7,43 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int gcd(int num1,int num2)
+{
+    int gcd=1;
+    
+    if ( num2 > num1) {
+            int temp = num2;
+            num2 = num1;
+            num1 = temp;
+        }
+        
+        for (int i = 1; i <=  num2; ++i) {
+            if (num1 % i == 0 && num2 % i ==0) {
+               
+                gcd=i;
+            }
+        }
+    return gcd;
+    
+}
+
+int main()
+{
+    int n1,n2;
+    
+    
+   
+    
+    cout<<"enter first number here:"<<endl;
+    cin>>n1;
+    
+    cout<<"enter second number here:"<<endl;
+    cin>>n2;
+    
+    
+    cout<<"GCD is:"<<gcd(n1,n2)<<endl;
+    
     return 0;
 }
